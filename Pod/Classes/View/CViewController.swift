669f2ca057d929f986f461d5543ec8881ebfa06f
ApterKing <wangcccong@outlook.com>
2017-05-26 13:49:16 +0800

    修改View

--- Pod/Classes/View/CViewController.swift
+++ Pod/Classes/View/CViewController.swift
@@ -8,24 +8,23 @@
 
 import UIKit
 
-class CViewController: UIViewController {
+public class CViewController: UIViewController {
 
-    override func viewDidLoad() {
+    public override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         self.title = "组件C"
-        
+
         let label = UILabel(frame: CGRect(x: (UIScreen.main.bounds.size.width - 120) / 2.0, y: 200, width: 120, height: 50))
         label.text = "这里是组件C测试"
         self.view.addSubview(label)
     }
 
-    override func didReceiveMemoryWarning() {
+    public override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
     /*
     // MARK: - Navigation
